0e6c8b9db05dbf4c3041f5a7494f1d0a40caa5ff
Washida1224 <146411267+Washida1224@users.noreply.github.com>
2024-10-17 21:23:57 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
+const wss = new WebSocket.Server({ port: process.env.PORT || 10000 });
 
 wss.on('connection', (ws) => {
     ws.on('message', (message) => {
