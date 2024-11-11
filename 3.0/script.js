c5ae87370eaf2af3d8cf3194b4c46de620068f23
Washida1224 <146411267+Washida1224@users.noreply.github.com>
2024-11-12 00:15:28 +0900

    Update script.js

--- 3.0/script.js
+++ 3.0/script.js
@@ -23,7 +23,7 @@ document.getElementById('logout-button').addEventListener('click', function(even
     window.location.href = 'index.html';
 });
 const detailsHTML = [
-    '<iframe src="https://washida1224.github.io/ProjectBC/3.0/組み込み4.html" width="100%" height="800" frameborder="0" marginheight="0" marginwidth="0"></iframe>',
+    '<iframe src="https://washida1224.github.io/ProjectBC/3.0/組み込み4.html" width="100%" height="900" frameborder="0" marginheight="0" marginwidth="0"></iframe>',
     '<iframe src="https://washida1224.github.io/ProjectBC/3.0/組み込み2.html" width="100%" height="800" frameborder="0" marginheight="0" marginwidth="0"></iframe>',
     "<h4>質問フォームの詳細</h4><p>質問フォームへのボタンを置く。他に書くこと思いつかねー</p>",
     "<h4>確認テストの詳細</h4><p>授業動画の内容についてのテスト。理解度をチェックしましょう的なことを書く。</p>",
